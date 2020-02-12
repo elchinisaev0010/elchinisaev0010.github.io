d3051526ec04b307f2288262ce4f91eb9a4b4ed1
elchinisaev0010 <60928756+elchinisaev0010@users.noreply.github.com>
2020-02-12 19:59:47 +1000

    Add files via upload

--- learn/JS/script.js
+++ learn/JS/script.js
@@ -17,6 +17,44 @@ function fun2(){
 			alert(' выбран ' +i+ ' элемент ');
 		}
 
-	}
-	
+	}	
 }
+
+function fun1(){
+	var	rtl=document.getElementById('rtl');
+	var	rtr=document.getElementById('rtr');
+	var	rbr=document.getElementById('rbr');
+	var	rbl=document.getElementById('rbl');
+	var	ttl=document.getElementById('ttl');
+	var	ttr=document.getElementById('ttr');
+	var	tbr=document.getElementById('tbr');
+	var	tbl=document.getElementById('tbl');
+
+	var	block = document.getElementById('block');
+
+	 ttl.value=rtl.value;
+	 ttr.value=rtr.value;
+	 tbr.value=rbr.value;
+	 tbl.value=rbl.value;
+
+	block.style.borderRadius=rtl.value + "px " + rtr.value + "px " + rbr.value + "px " + rbl.value + "px ";
+}
+// function fun1() {
+//  var rtl=document.getElementById('rtl');
+//  var rtr=document.getElementById('rtr');
+//  var rbr=document.getElementById('rbr');
+//  var rbl=document.getElementById('rbl');
+//  var ttl=document.getElementById('ttl');
+//  var ttr=document.getElementById('ttr');
+//  var tbr=document.getElementById('tbr');
+//  var tbl=document.getElementById('tbl');
+
+//  var block =document.getElementById('block');
+ 
+//  ttl.value=rtl.value;
+//  ttr.value=rtr.value;
+//  tbr.value=rbr.value;
+//  tbl.value=rbl.value;
+
+// block.style.borderRadius=rtl.value + "px " + rtr.value + "px " + rbr.value + "px " + rbl.value + "px ";
+// }
